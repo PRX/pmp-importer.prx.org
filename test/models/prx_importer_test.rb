65acd5b3a5e7287cdf122aa92f92673b3ce59eac
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-08-13 17:53:29 -0400

    remove puts

--- test/models/prx_importer_test.rb
+++ test/models/prx_importer_test.rb
@@ -57,7 +57,7 @@ describe PRXImporter do
         # delete to clean up prior runs
         delete_count = 0
         prx_importer.pmp.query["urn:collectiondoc:query:docs"].where(tag: 'prx_test', limit: 100).items.each{|i| i.delete; delete_count+=1 }
-        puts "deleted #{delete_count}"
+        # puts "deleted #{delete_count}"
 
       end
 
