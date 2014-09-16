2bef0bcbaf4ab2ca63a0ffbec2466680598da321
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-09-16 16:02:49 -0400

    get rid of sandbox, fix story title

--- test/models/prx_importer_test.rb
+++ test/models/prx_importer_test.rb
@@ -46,7 +46,9 @@ describe PRXImporter do
 
     it 'wraps urls with count redirect' do
       url = prx_importer.count_audio_url('/audio_files/blah/1/test.mp3', 123, 456, 'thisis-aguid-fortesting')
-      url.must_equal "https://count.prx.org/redirect?action=request&action_value=%7B%22audioFileId%22%3A123%2C%22pieceId%22%3A456%7D&location=https%3A%2F%2Fhal.prx.org%2Faudio_files%2Fblah%2F1%2Ftest.mp3&referrer=https%3A%2F%2Fapi.pmp.io%2Fdocs%2Fthisis-aguid-fortesting"
+      if use_webmock?
+        url.must_equal "https://count.prx.org/redirect?action=request&action_value=%7B%22audioFileId%22%3A123%2C%22pieceId%22%3A456%7D&location=https%3A%2F%2Fhal.prx.org%2Faudio_files%2Fblah%2F1%2Ftest.mp3&referrer=https%3A%2F%2Fapi.pmp.io%2Fdocs%2Fthisis-aguid-fortesting"
+      end
     end
   end
 
@@ -101,7 +103,7 @@ describe PRXImporter do
           to_return(:status => 200, :body => "", :headers => {})
 
         # create account
-        stub_request(:put, "https://publish-sandbox.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b2").
+        stub_request(:put, "https://publish.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b2").
           with(:body => "{\"links\":{\"profile\":[{\"href\":\"https://api.pmp.io/profiles/property\",\"type\":\"application/vnd.collection.doc+json\"}],\"alternate\":[{\"href\":\"https://www.prx.org/group_accounts/45139\"}]},\"attributes\":{\"guid\":\"9ff6db7a-93e6-4987-9313-4d70d74051b2\",\"title\":\"The Moth\",\"tags\":[\"prx_test\",\"PRX\",\"_prx_accounts__45139_\"]}}").
           to_return(:status => 200, :body => '{"url":"https://api.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b2"}', :headers => {})
 
@@ -124,12 +126,12 @@ describe PRXImporter do
           to_return(:status => 200, :body => "", :headers => {})
 
         # create image for series
-        stub_request(:put, "https://publish-sandbox.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b4").
+        stub_request(:put, "https://publish.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b4").
           with(:body => "{\"links\":{\"profile\":[{\"href\":\"https://api.pmp.io/profiles/image\",\"type\":\"application/vnd.collection.doc+json\"}],\"enclosure\":[{\"href\":\"https://hal.prx.org/pub/e56ce22b1bce78de79993ebcccf76611/0/web/series_image/8696/medium/WEEKLY_LOGO.jpg\",\"type\":\"image/jpeg\",\"meta\":{\"crop\":\"medium\"}}]},\"attributes\":{\"guid\":\"9ff6db7a-93e6-4987-9313-4d70d74051b4\",\"title\":\"\",\"byline\":\"\",\"tags\":[\"prx_test\",\"PRX\",\"_prx_series_images__8696_\"]}}").
           to_return(:status => 200, :body => '{"url":"https://api.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b4"}', :headers => {})
 
         # create property for series
-        stub_request(:put, "https://publish-sandbox.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b3").
+        stub_request(:put, "https://publish.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b3").
           with(:body => "{\"links\":{\"profile\":[{\"href\":\"https://api.pmp.io/profiles/series\",\"type\":\"application/vnd.collection.doc+json\"}],\"alternate\":[{\"href\":\"https://www.prx.org/series/32832\"}],\"item\":[{\"href\":\"https://api.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b4\",\"title\":\"\",\"rels\":[\"urn:collectiondoc:image\"]}]},\"attributes\":{\"guid\":\"9ff6db7a-93e6-4987-9313-4d70d74051b3\",\"title\":\"Moth Weekly Episodes for 2013\",\"tags\":[\"prx_test\",\"PRX\",\"_prx_series__32832_\"]}}").
           to_return(:status => 200, :body => '{"url":"https://api.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b3"}', :headers => {})
 
@@ -144,7 +146,7 @@ describe PRXImporter do
           to_return(:status => 200, :body => "", :headers => {})
 
         # create image for story
-        stub_request(:put, "https://publish-sandbox.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b5").
+        stub_request(:put, "https://publish.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b5").
           with(:body => "{\"links\":{\"profile\":[{\"href\":\"https://api.pmp.io/profiles/image\",\"type\":\"application/vnd.collection.doc+json\"}],\"enclosure\":[{\"href\":\"https://hal.prx.org/pub/f8b82b49a679ab9a621791bc9b752ff2/0/web/story_image/203874/medium/Moth_ElnaBaker_1301.jpg\",\"type\":\"image/jpeg\",\"meta\":{\"crop\":\"medium\"}}]},\"attributes\":{\"guid\":\"9ff6db7a-93e6-4987-9313-4d70d74051b5\",\"title\":\"Elna Baker\",\"byline\":\"Elna Baker\",\"tags\":[\"prx_test\",\"PRX\",\"_prx_story_images__203874_\"]}}").
           to_return(:status => 200, :body => '{"url":"https://api.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b5"}', :headers => {})
 
@@ -159,15 +161,15 @@ describe PRXImporter do
           to_return(:status => 200, :body => "", :headers => {})
 
         # create audio
-        stub_request(:put, "https://publish-sandbox.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b6").
+        stub_request(:put, "https://publish.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b6").
           with(:body => "{\"links\":{\"profile\":[{\"href\":\"https://api.pmp.io/profiles/audio\",\"type\":\"application/vnd.collection.doc+json\"}],\"enclosure\":[{\"href\":\"https://count.prx.org/redirect?action=request\\u0026action_value=%7B%22audioFileId%22%3A451642%2C%22pieceId%22%3A87683%7D\\u0026location=https%3A%2F%2Fhal.prx.org%2Fpub%2F472875466d225aca0480000fea4b5fc2%2F0%2Fweb%2Faudio_file%2F451642%2Fbroadcast%2FMoth1301GarrisonFinal.mp3\\u0026prx_story_id=87683\\u0026referrer=https%3A%2F%2Fapi.pmp.io%2Fdocs%2F9ff6db7a-93e6-4987-9313-4d70d74051b6\",\"type\":\"audio/mpeg\",\"meta\":{\"duration\":3179,\"size\":101617830}}]},\"attributes\":{\"guid\":\"9ff6db7a-93e6-4987-9313-4d70d74051b6\",\"title\":\"Moth 1301 Single File\",\"tags\":[\"prx_test\",\"PRX\",\"_prx_audio_files__451642_\"]}}").
           to_return(:status => 200, :body => '{"url":"https://api.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b6"}', :headers => {})
 
         # ... Story Finish
 
         # create story
-        stub_request(:put, "https://publish-sandbox.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b1").
-          with(:body => "{\"links\":{\"profile\":[{\"href\":\"https://api.pmp.io/profiles/story\",\"type\":\"application/vnd.collection.doc+json\"}],\"collection\":[{\"href\":\"https://api.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b2\",\"title\":\"The Moth\",\"rels\":[\"urn:collectiondoc:collection:property\"]},{\"href\":\"https://api.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b3\",\"title\":\"Moth Weekly Episodes for 2013\",\"rels\":[\"urn:collectiondoc:collection:series\"]}],\"item\":[{\"href\":\"https://api.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b5\",\"title\":\"Elna Baker\",\"rels\":[\"urn:collectiondoc:image\"]},{\"href\":\"https://api.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b6\",\"title\":\"Moth 1301 Single File\",\"rels\":[\"urn:collectiondoc:audio\"]}],\"alternate\":[{\"href\":\"https://www.prx.org/pieces/87683\"}]},\"attributes\":{\"guid\":\"9ff6db7a-93e6-4987-9313-4d70d74051b1\",\"hreflang\":\"en\",\"title\":\"The Moth Radio Hour\",\"teaser\":\"A special live edition of The Moth at Town Hall in NYC with Garrison Keillor, with additional hosting by Jay Allison. World renowned conservationist and field biologist Alan Rabinowitz (\\\"Man and Beast\\\", MRH episode 902) makes a life-changing friendship while studying the Taron tribe of the Himilayas; a woman describes how her father’s risky entrepreneurial endeavors kept her close-knit Southern family in flux; and a woman raised as a Mormon is terrified that her parents will disown her when she confesses that she has left the faith. \\n\\n\\n \\n\",\"description\":\"Tina McElroy Ansa is a little girl when her father\\u0026rsquo;s business goes under and her family must leave their beloved, expansive home.Alan Rabinowitz treks through the Himilayas to study the Taron, a dying race of people, and makes discoveries about himself.\\u0026nbsp;\\nElna Baker must tell her Mormon parents that she has made an irreversible change in her life.\\n\\u0026nbsp;Hosted by Jay Allison/Garrison Keillor\\u0026nbsp;\",\"contentencoded\":\"\\u003cdiv\\u003eTina McElroy Ansa is a little girl when her father\\u0026rsquo;s business goes under and her family must leave their beloved, expansive home.\\u003cbr /\\u003e\\u003cbr /\\u003eAlan Rabinowitz treks through the Himilayas to study the Taron, a dying race of people, and makes discoveries about himself.\\u003cbr /\\u003e\\u0026nbsp;\\u003c/div\\u003e\\n\\u003cdiv\\u003eElna Baker must tell her Mormon parents that she has made an irreversible change in her life.\\u003c/div\\u003e\\n\\u003cdiv\\u003e\\u0026nbsp;\\u003cbr /\\u003eHosted by Jay Allison/Garrison Keillor\\u0026nbsp;\\u003c/div\\u003e\",\"byline\":\"The Moth\",\"published\":\"2012-12-20T18:57:45.000+00:00\",\"valid\":{\"from\":\"2012-12-20T18:57:45.000+00:00\",\"to\":\"3012-12-20T18:57:45.000+00:00\"},\"tags\":[\"prx_test\",\"PRX\",\"_prx_stories__87683_\"]}}").
+        stub_request(:put, "https://publish.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b1").
+          with(:body => "{\"links\":{\"profile\":[{\"href\":\"https://api.pmp.io/profiles/story\",\"type\":\"application/vnd.collection.doc+json\"}],\"collection\":[{\"href\":\"https://api.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b2\",\"title\":\"The Moth\",\"rels\":[\"urn:collectiondoc:collection:property\"]},{\"href\":\"https://api.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b3\",\"title\":\"Moth Weekly Episodes for 2013\",\"rels\":[\"urn:collectiondoc:collection:series\"]}],\"item\":[{\"href\":\"https://api.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b5\",\"title\":\"Elna Baker\",\"rels\":[\"urn:collectiondoc:image\"]},{\"href\":\"https://api.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b6\",\"title\":\"Moth 1301 Single File\",\"rels\":[\"urn:collectiondoc:audio\"]}],\"alternate\":[{\"href\":\"https://www.prx.org/pieces/87683\"}]},\"attributes\":{\"guid\":\"9ff6db7a-93e6-4987-9313-4d70d74051b1\",\"hreflang\":\"en\",\"title\":\"Virginity, Fidelity, and Fertility\",\"teaser\":\"A special live edition of The Moth at Town Hall in NYC with Garrison Keillor, with additional hosting by Jay Allison. World renowned conservationist and field biologist Alan Rabinowitz (\\\"Man and Beast\\\", MRH episode 902) makes a life-changing friendship while studying the Taron tribe of the Himilayas; a woman describes how her father’s risky entrepreneurial endeavors kept her close-knit Southern family in flux; and a woman raised as a Mormon is terrified that her parents will disown her when she confesses that she has left the faith. \\n\\n\\n \\n\",\"description\":\"Tina McElroy Ansa is a little girl when her father\\u0026rsquo;s business goes under and her family must leave their beloved, expansive home.Alan Rabinowitz treks through the Himilayas to study the Taron, a dying race of people, and makes discoveries about himself.\\u0026nbsp;\\nElna Baker must tell her Mormon parents that she has made an irreversible change in her life.\\n\\u0026nbsp;Hosted by Jay Allison/Garrison Keillor\\u0026nbsp;\",\"contentencoded\":\"\\u003cdiv\\u003eTina McElroy Ansa is a little girl when her father\\u0026rsquo;s business goes under and her family must leave their beloved, expansive home.\\u003cbr /\\u003e\\u003cbr /\\u003eAlan Rabinowitz treks through the Himilayas to study the Taron, a dying race of people, and makes discoveries about himself.\\u003cbr /\\u003e\\u0026nbsp;\\u003c/div\\u003e\\n\\u003cdiv\\u003eElna Baker must tell her Mormon parents that she has made an irreversible change in her life.\\u003c/div\\u003e\\n\\u003cdiv\\u003e\\u0026nbsp;\\u003cbr /\\u003eHosted by Jay Allison/Garrison Keillor\\u0026nbsp;\\u003c/div\\u003e\",\"byline\":\"The Moth\",\"published\":\"2012-12-20T18:57:45.000+00:00\",\"valid\":{\"from\":\"2012-12-20T18:57:45.000+00:00\",\"to\":\"3012-12-20T18:57:45.000+00:00\"},\"tags\":[\"prx_test\",\"PRX\",\"_prx_stories__87683_\"]}}").
           to_return(:status => 200, :body => '{"url":"https://api.pmp.io/docs/9ff6db7a-93e6-4987-9313-4d70d74051b1"}', :headers => {})
 
         # stub the guid to a predictable value
@@ -188,10 +190,15 @@ describe PRXImporter do
       else
 
         # delete to clean up prior runs
+        pi = PRXImporter.new
         delete_count = 0
-        prx_importer.pmp.query["urn:collectiondoc:query:docs"].where(tag: 'prx_test', limit: 100).items.each{|i| i.delete; delete_count+=1 }
-        # puts "deleted #{delete_count}"
 
+        puts "delete prx_test: #{pi.pmp.inspect}"
+        items = pi.pmp.query["urn:collectiondoc:query:docs"].where(tag: 'prx_test', limit: 100).items
+        puts "items: #{items.inspect}"
+        
+        items.each{|i| puts i.to_json; i.delete; delete_count+=1 }
+        puts "deleted #{delete_count}"
       end
 
     }
@@ -201,7 +208,7 @@ describe PRXImporter do
     it "imports a prx piece" do
       doc = prx_importer.import(prx_story_id: 87683)
 
-      doc.title.must_equal "The Moth Radio Hour"
+      doc.title.must_equal "Virginity, Fidelity, and Fertility"
       doc.tags.sort.must_equal  ["PRX", "_prx_stories__87683_", "prx_test"]
 
       doc.links['profile'].href.must_match /story$/
