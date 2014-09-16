bca6e931cfb9bb79abcbc7dcd34569006e5988f1
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-09-16 10:11:05 -0400

    break out condition for debugging

--- app/models/pmp_guid_mapping.rb
+++ app/models/pmp_guid_mapping.rb
@@ -7,8 +7,12 @@ class PMPGuidMapping < ActiveRecord::Base
 
   def self.find_or_create_guid(name, type, id)
     conditions = { source_name: name, source_type: type, source_id: id }
+    # puts "\n\nfind_or_create_guid conditions 1: #{conditions.inspect}"
     guid_mapping = PMPGuidMapping.where(conditions).first
-    guid_mapping = PMPGuidMapping.create!(conditions.merge(guid: new_guid)) unless guid_mapping
+
+    conditions = conditions.merge(guid: new_guid)
+    # puts "\n\nfind_or_create_guid conditions 2: #{conditions.inspect}"
+    guid_mapping = PMPGuidMapping.create!(conditions) unless guid_mapping
     guid_mapping.guid
   end
 
