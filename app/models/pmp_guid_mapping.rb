933e576a898d671de2b59920b356f571addede7f
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-10-01 17:09:38 -0400

    use to_s on params, and more explicit call for new guid

--- app/models/pmp_guid_mapping.rb
+++ app/models/pmp_guid_mapping.rb
@@ -1,18 +1,18 @@
 class PMPGuidMapping < ActiveRecord::Base
 
   def self.find_guid(name, type, id)
-    guid_mapping = PMPGuidMapping.where(source_name: name, source_type: type, source_id: id.to_s).first
-    guid_mapping ? guid_mapping.guid : nil
+    guid_mapping = PMPGuidMapping.where(source_name: name.to_s, source_type: type.to_s, source_id: id.to_s).first
+    guid_mapping.try(:guid)
   end
 
   def self.find_or_create_guid(name, type, id)
-    conditions = { source_name: name, source_type: type, source_id: id.to_s }
-    # puts "\n\nfind_or_create_guid conditions 1: #{conditions.inspect}"
+    puts "find_or_create_guid: #{name}, #{type}, #{id}"
+
+    conditions = { source_name: name.to_s, source_type: type.to_s, source_id: id.to_s }
     guid_mapping = PMPGuidMapping.where(conditions).first
 
-    conditions = conditions.merge(guid: new_guid)
-    # puts "\n\nfind_or_create_guid conditions 2: #{conditions.inspect}"
-    guid_mapping = PMPGuidMapping.create!(conditions) unless guid_mapping
+    guid_mapping ||= PMPGuidMapping.create!(conditions.merge(guid: PMPGuidMapping.new_guid))
+
     guid_mapping.guid
   end
 
