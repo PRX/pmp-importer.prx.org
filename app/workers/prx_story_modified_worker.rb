5ccc70fdd15081db55278e8335a7b8b78cf3b571
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-11-14 15:02:30 -0500

    add in sidekiq

---
+++ app/workers/prx_story_modified_worker.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+class PRXStoryModifiedWorker
+  include Sidekiq::Worker
+
+  sidekiq_options retry: 5, backtrace: true
+
+  def process(story_id)
+    prx = PRXImporter.new.import(prx_story_id: story_id)
+  end
+
+end
