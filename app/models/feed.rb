ba296b6fda0b51fe9f962a4e6392bc34648a9a35
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-11-25 17:53:59 -0500

    add methods to update models from the parsed podcast

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -6,17 +6,62 @@ class Feed < ActiveRecord::Base
   has_many :responses, class_name: 'FeedResponse'
   has_many :entries, class_name: 'FeedEntry'
 
-  serialize :options
+  serialize :options, JSON
 
   def sync(force=false)
     return unless response = updated_response(force)
 
     feed = Feedjira::Feed.parse(response.body)
-    feed_importer = FeedImporter.new(options || {})
+    update_feed(feed)
 
     feed.entries.each do |entry|
       insert_or_update_entry(entry)
     end
+  end
+
+  def parse_categories(feed)
+    icat = Array(feed.itunes_categories).map(&:strip).select{|c| !c.blank?}
+    mcat = Array(feed.media_categories)
+    rcat = Array(feed.categories)
+    (icat + mcat + rcat).compact.uniq
+  end
+
+  def parse_keywords(feed)
+    ikey = Array(feed.itunes_keywords).map(&:strip)
+    mkey = Array(feed.media_keywords).map(&:strip)
+    (ikey + mkey).compact.uniq
+  end
+
+  def update_feed(feed)
+    self.author           = feed.itunes_author
+    self.block            = (feed.itunes_block == 'yes')
+    self.categories       = parse_categories(feed)
+    self.complete         = (feed.itunes_complete == 'yes')
+    self.copyright        = feed.copyright || feed.media_copyright
+    self.description      = feed.description
+    self.explicit         = (feed.explicit && feed.explicit != 'no')
+    self.feedburner_name  = feed.feedburner_name
+    self.generator        = feed.generator
+    self.hub_url          = Array(feed.hubs).first
+    self.image_url        = feed.itunes_image || feed.image.try(:url)
+    self.keywords         = parse_keywords(feed)
+    self.language         = feed.language
+    self.last_built       = feed.last_built
+    self.last_modified    = feed.last_modified
+    self.managing_editor  = feed.managing_editor
+    self.new_feed_url     = feed.itunes_new_feed_url
+    self.owners           = feed.itunes_owners.collect{|o| {name: o.name, email: o.email}}
+    self.pub_date         = feed.pub_date
+    self.published        = feed.published
+    self.subtitle         = feed.itunes_subtitle
+    self.summary          = feed.itunes_summary
+    self.ttl              = feed.ttl
+    self.update_frequency = feed.update_frequency
+    self.update_period    = feed.update_period
+    self.url              = feed.url
+    self.web_master       = feed.web_master
+
+    self
   end
 
   def insert_or_update_entry(entry)
@@ -60,20 +105,26 @@ class Feed < ActiveRecord::Base
   end
 
   def validate_response(last_response)
+    http_response = feed_http_response(last_response)
+    feed_response = FeedResponse.for_response(http_response)
+
+    if feed_response.not_modified?
+      feed_response = nil
+    else
+      self.responses << feed_response
+    end
+
+    feed_response
+  end
+
+  def feed_http_response(last_response=nil)
     http_response = connection.get do |req|
       req.url uri.path
-      req.headers['If-Modified-Since'] = last_response.last_modified if last_response.last_modified
-      req.headers['If-None-Match']     = last_response.etag if last_response.etag
+      req.headers['If-Modified-Since'] = last_response.last_modified if last_response.try(:last_modified)
+      req.headers['If-None-Match']     = last_response.etag if last_response.try(:etag)
     end
 
-    response = FeedResponse.for_response(http_response)
-    if response.not_modified?
-      response = nil
-    else
-      self.responses << response
-    end
-
-    response
+    http_response
   end
 
   def uri
--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -3,6 +3,8 @@ class FeedEntry < ActiveRecord::Base
 
   after_commit :process_feed_entry
 
+  serialize :categories, JSON
+
   def process_feed_entry
     FeedEntryModifiedWorker.perform_async(self.id)
   end
@@ -23,28 +25,43 @@ class FeedEntry < ActiveRecord::Base
 
   def update_with_entry(entry)
     return unless is_changed?(entry)
-    entry.set_attributes(entry).save
+    entry.update_feed_entry(entry).save
   end
 
-  def set_attributes(entry)
+  def update_feed_entry(entry)
     self.digest           = FeedEntry.entry_digest(entry)
 
     self.author           = entry[:itunes_author] || entry[:author]
+    self.block            = (entry[:itunes_block] == 'yes')
+    self.categories       = entry[:categories]
+
+    self.comment_count    = entry[:comment_count]
+    self.comment_rss_url  = entry[:comment_rss_url]
+    self.comment_url      = entry[:comments]
     self.content          = entry[:content]
+    self.description      = entry[:description]
     self.duration         = seconds_for_duration(entry[:itunes_duration] || entry[:duration])
-    self.enclosure_length = entry[:enclosure_length]
-    self.enclosure_type   = entry[:enclosure_type]
-    self.enclosure_url    = entry[:enclosure_url]
     self.entry_id         = entry[:entry_id]
-    self.explicit         = (entry[:itunes_explicit] && entry[:itunes_explicit] != 'no')
+    self.explicit         = entry[:itunes_explicit]
+    self.feedburner_orig_enclosure_link = entry[:feedburner_orig_enclosure_link]
+    self.feedburner_orig_link = entry[:feedburner_orig_link]
     self.image_url        = entry[:itunes_image]
-    self.keywords         = entry[:itunes_keywords]
+    self.is_closed_captioned = (entry[:itunes_is_closed_captioned] == 'yes')
+    self.keywords         = (entry[:itunes_keywords] || '').split(',').map(&:strip)
+    self.position         = entry[:itunes_order]
     self.published        = entry[:published]
     self.subtitle         = entry[:itunes_subtitle]
-    self.summary          = entry[:itunes_summary] || entry[:summary]
+    self.summary          = entry[:itunes_summary]
     self.title            = entry[:title]
     self.updated          = entry[:updated]
     self.url              = entry[:url]
+
+    # TODO: do something with media_groups/media_contents if no enclosure
+    if entry[:enclosure]
+      self.enclosure_length = entry[:enclosure].length
+      self.enclosure_type   = entry[:enclosure].type
+      self.enclosure_url    = entry[:enclosure].url
+    end
 
     self
   end
