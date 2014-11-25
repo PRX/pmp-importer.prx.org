38043c0f269309f437ebb05d7b2dbdcd02accdfe
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-11-25 17:52:17 -0500

    fix function name on worker

--- app/workers/feed_entry_modified_worker.rb
+++ app/workers/feed_entry_modified_worker.rb
@@ -3,7 +3,7 @@ class FeedEntryModifiedWorker
 
   sidekiq_options retry: 5, backtrace: true
 
-  def process(feed_entry_id)
+  def perform(feed_entry_id)
     ActiveRecord::Base.connection_pool.with_connection do
       entry = FeedEntry.find(feed_entry_id)
       FeedImporter.new.import(feed_entry_id: feed_entry_id)
